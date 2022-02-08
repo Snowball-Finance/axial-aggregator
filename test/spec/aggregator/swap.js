843fb0b3883ed8fdcd38e9238ce86e56205834a0
Bayological <6872903+bayological@users.noreply.github.com>
2022-02-08 14:07:15 -0500

    test: add additional swap test cases

--- test/spec/aggregator/swap.js
+++ test/spec/aggregator/swap.js
@@ -3,22 +3,31 @@ const { expect } = require("chai")
 
 const fixtures = require('../../fixtures')
 const helpers = require('../../helpers') 
+const addresses = require('../../addresses.json')
 
-describe('Axial Aggregator - Swap', () => {
+const { assets } = addresses
+
+describe.only('Axial Aggregator - Swap', () => {
 
     let fix;
-    let internalRouterAddress;
-    let externalRouterAddress;
+
+    const testCases = [
+        {tokenIn: assets.DAIe, tokenInSymbol: 'DAIe' , tokenOut: assets.FRAXc, tokenOutSymbol: 'FRAX' },
+        {tokenIn: assets.TSD, tokenInSymbol: 'TSD' , tokenOut: assets.AVAI, tokenOutSymbol: 'AVAI' },
+        {tokenIn: assets.FRAXc, tokenInSymbol: 'FRAX' , tokenOut: assets.AVAI, tokenOutSymbol: 'AVAI' },
+        {tokenIn: assets.USDCe, tokenInSymbol: 'USDCe' , tokenOut: assets.TSD, tokenOutSymbol: 'TSD' },
+        {tokenIn: assets.USDTe, tokenInSymbol: 'USDTe' , tokenOut: assets.USDC, tokenOutSymbol: 'USDC' },
+        {tokenIn: assets.USDTe, tokenInSymbol: 'USDTe' , tokenOut: assets.MIM, tokenOutSymbol: 'MIM' },
+        {tokenIn: assets.AVAI, tokenInSymbol: 'AVAI' , tokenOut: assets.TUSD, tokenOutSymbol: 'TUSD' },
+    ]
 
     before(async () => {
-        fix = await fixtures.general()
-        const fixRouter = await fixtures.router()
-        assets = fix.assets
-        owner = fix.deployer
-        AxialAggregator = fixRouter.AxialAggregator
-
-        internalRouterAddress = await AxialAggregator.InternalRouter();
-        externalRouterAddress = await AxialAggregator.ExternalRouter();
+        fix = await fixtures.general();
+        const fixRouter = await fixtures.router(); 
+        owner = fix.deployer;
+        AxialAggregator = fixRouter.AxialAggregator;
+        AxialRouter = fixRouter.AxialRouter;
+        InternalRouter = fixRouter.InternalAxialRouter;
     })
 
     beforeEach(async () => {
@@ -26,61 +35,63 @@ describe('Axial Aggregator - Swap', () => {
         trader = fix.genNewAccount()
     })
 
-    it.only('Should return path using internal router and swap TSD to AVAI with correct amounts', async () => { 
-        
-        let amountIn = ethers.utils.parseUnits('10');
-        let tokenIn = assets.TSD;
-        let tokenOut = assets.AVAI;
-        let steps = 3
+    testCases.forEach(testCase => {
+        it(`Should return path using internal router and swap ${testCase.tokenInSymbol} to ${testCase.tokenOutSymbol} with correct amounts`, async () => { 
+            let amountIn = ethers.utils.parseUnits('1', 6)
+            let tokenIn = testCase.tokenIn;
+            let tokenOut = testCase.tokenOut;
+            let steps = 4
 
-        // Top up trader
-        let [ topUpAmountIn ] = await fix.PangolinRouter.getAmountsIn(amountIn, [assets.WAVAX, assets.TSD])
-        await fix.PangolinRouter.connect(trader).swapAVAXForExactTokens(
-            amountIn, 
-            [assets.WAVAX, assets.TSD], 
-            trader.address, 
-            parseInt(Date.now()/1e3)+3000, 
-            { value: topUpAmountIn }
-        ).then(response => response.wait())
+            // Top up trader
+            let [ topUpAmountIn ] = await fix.PangolinRouter.getAmountsIn(amountIn, [assets.WAVAX, tokenIn])
+            await fix.PangolinRouter.connect(trader).swapAVAXForExactTokens(
+                amountIn, 
+                [assets.WAVAX, tokenIn], 
+                trader.address, 
+                parseInt(Date.now()/1e3)+3000, 
+                { value: topUpAmountIn }
+            ).then(response => response.wait())
 
-        // Query for best path
-        let query = await AxialAggregator.findBestPath([
-            amountIn, 
-            tokenIn, 
-            tokenOut, 
-            steps
-        ]);
+            // Query for best path
+            let query = await AxialAggregator.findBestPath([
+                amountIn, 
+                tokenIn, 
+                tokenOut, 
+                steps
+            ]);
 
-        // Should use internal router
-        expect(query.useInternalRouter).to.be.true; 
-        
-        // Path should be correct
-        expect(query.bestPath.path[0]).to.be.equal(assets.TSD);
-        expect(query.bestPath.path[query.bestPath.path.length-1]).to.be.equal(assets.AVAI);
+            // Should use internal router
+            expect(query.useInternalRouter).to.be.true; 
+            
+            // Path should be correct
+            expect(query.bestPath.path[0]).to.be.equal(tokenIn);
+            expect(query.bestPath.path[query.bestPath.path.length-1]).to.be.equal(tokenOut);
 
-        // Adapters should be correct
-        expect(query.bestPath.adapters.length).to.gte(2);
+            // Should have adapters
+            expect(query.bestPath.adapters.length).to.gt(0);
 
-        // Approve internal rotuer
-        await helpers.approveERC20(trader, tokenIn, internalRouterAddress, ethers.constants.MaxUint256);
+            // Approve Aggregator
+            await helpers.approveERC20(trader, tokenIn, AxialAggregator.address, ethers.constants.MaxUint256);
 
-        // Do swap
-        const swap = () => AxialAggregator.connect(trader).swap(
-            [
-                query.bestPath.amounts[0], 
-                query.bestPath.amounts[query.bestPath.amounts.length-1],
-                query.bestPath.path,
-                query.bestPath.adapters
-            ],
-            trader.address, 
-            0,
-            query.useInternalRouter
-        )
+            // Do swap
+            const swap = () => AxialAggregator.connect(trader).swap(
+                [
+                    amountIn, 
+                    query.bestPath.amounts[query.bestPath.amounts.length-1],
+                    query.bestPath.path,
+                    query.bestPath.adapters
+                ],
+                trader.address, 
+                0,
+                query.useInternalRouter
+            );
 
-        const tokenOutContract = await ethers.getContractAt('contracts/interface/IERC20.sol:IERC20', tokenOut)
-        const expectedOutAmount = query.bestPath.amounts[query.bestPath.amounts.length-1]
-        await expect(swap).to.changeTokenBalance(
-            tokenOutContract, trader, expectedOutAmount
-        )
-    })
+            const tokenOutContract = await ethers.getContractAt('contracts/interface/IERC20.sol:IERC20', tokenOut)
+            const expectedOutAmount = query.bestPath.amounts[query.bestPath.amounts.length-1]
+
+            await expect(swap).to.changeTokenBalance(
+                tokenOutContract, trader, expectedOutAmount
+            )
+        });    
+    }); 
 })
